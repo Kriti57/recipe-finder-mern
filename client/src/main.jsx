a470c1a3dbcd997099ca06de39e1ba2fdbb24eb8
Kriti Gupta <kriti.gupta20004@gmail.com>
2026-06-05 14:57:10 +0530

    fix: update AuthProvider import in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './context/AuthProvider';
 
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
